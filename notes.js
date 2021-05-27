21c309fc708f5fdd05a171411fb77fb4fdca72e9
eschnei2 <everettschneider@gmail.com>
2021-05-27 11:17:50 -0500

    First amount of notes

---
+++ notes.js
@@ -0,0 +1,18 @@
+const notes = [
+    {
+        id: 1,
+        subject: "objects in javascript",
+        date: "05/26/2021",
+        feeling: "hungry",
+        timeSpent: " 10 mins"
+    },
+    {
+        id: 2,
+        subject: "CSS",
+        date: "05/23/2021",
+        feeling: "interested",
+        timeSpint: "1000 mins"
+    }
+]
+
+console.log(notes)
