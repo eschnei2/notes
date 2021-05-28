d8626771d4f9b5b47b1b3f17feabe3c648329675
eschnei2 <everettschneider@gmail.com>
2021-05-28 14:44:02 -0500

    finished for now

--- notes.js
+++ notes.js
@@ -4,16 +4,37 @@ const notes = [
         subject: "objects in javascript",
         date: "05/26/2021",
         feeling: "hungry",
-        timeSpent: " 10 mins"
+        timeSpent: 10
     },
     {
         id: 2,
         subject: "CSS",
         date: "05/23/2021",
         feeling: "interested",
-        timeSpint: "1000 mins"
+        timeSpent: 1000
     }
 ]
+
+const createNote = (note) => {
+        const lastIndex = notes.length - 1
+        const currentLastNote = notes[lastIndex]
+        const maxId = currentLastNote.id
+        const idForNewNote = maxId + 1
+    
+        note.id = idForNewNote
+        notes.push(note)   
+}
+
+let today = new Date()
+let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate()
+
+const moreNewerNote = {
+    subject: "Date display",
+    feeling: "hungry",
+    timeSpent: 20
+}
+
+createNote(moreNewerNote)
 
 const searchTerm = "hungry"
 
@@ -23,7 +44,7 @@ for (const note of notes) {
     Subject is ${note.subject}
     Date learned: ${note.date}
     It made me feel ${note.feeling}
-    I spent ${note.timeSpint} on it
+    I spent ${note.timeSpent} minutes on it
     ------------------------------`)
     }
 }
